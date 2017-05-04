be64b6d123af1a1941db74c13e43c3f329d82a3f
Przemysław Kopański <P.Kopanski@stud.elka.pw.edu.pl>
2017-05-04 15:31:11 +0200

    Add commandline parsing
    Change-Id: I341da9fcbca4b1a14d8a28404cf84ef6788dbc83

--- main.cpp
+++ main.cpp
@@ -2,10 +2,43 @@
 // Created by Mateusz Forc on 03.05.17.
 //
 
+#include <unistd.h>
+#include <iostream>
 #include "Connector.h"
+
+extern char *optarg;
+
+using namespace std;
+
+
+void showUsage(const char* prog) {
+    cout << "Usage: " << prog
+         << " [-r configfile] | [-t unit|int] | -h" << endl;
+}
+
 
 int main(int argc, char** argv)
 {
+    int opt;
+    string stringParam;
+    opt = getopt(argc, argv, "r:t:h");
+    switch (opt) {
+      case 'r': {
+        stringParam = optarg;
+        break;
+      }
+      case 't': {
+        stringParam = optarg;
+        break;
+      }
+      default: {
+        showUsage(argv[0]);
+        exit(EXIT_FAILURE);
+        break;
+      }
+    }
+
     Connector connector;
     return 0;
 }
+
