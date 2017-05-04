bdea769fb89667793d5d6dbe0790f31e938a42d3
Wiktor Franus <wiktor515.95@gmail.com>
2017-05-04 18:21:35 +0200

    Add implementation of listen and send methods
    Change-Id: I86e11f49c8ba4d15ce6196df5b1f3c624b49f507

--- Connector.cpp
+++ Connector.cpp
@@ -29,12 +29,44 @@ Connector::Connector(size_t listenPort) : m_listenPort(listenPort) {
 }
 
 void Connector::send(const std::string &address, size_t port, const Message &msg) {
-    struct sockaddr_in6 name;
+    struct sockaddr_in6 sa6;
+    struct sockaddr_in sa;
 
-    name.sin6_family = AF_INET6;
-    inet_pton(AF_INET6, address.data(), &(name.sin6_addr));
-    name.sin6_port = htons(static_cast<uint16_t>(port));
+    sa.sin_family = AF_INET;
+    sa6.sin6_family = AF_INET6;
+    sa.sin_port = htons(static_cast<uint16_t>(port));
+    sa6.sin6_port = htons(static_cast<uint16_t>(port));
 
-    //if (sendto(m_listenSocket, (void*)msg, sizeof msg, 0,(struct sockaddr *) &name, sizeof name) == -1)
-    //    throw std::runtime_error("Sending message failed");
+    if (inet_pton(AF_INET, address.data(), &(sa.sin_addr)) != 1) {
+        if (inet_pton(AF_INET6, address.data(), &(sa6.sin6_addr)) != 1)
+            throw std::runtime_error("Unrecognized IP address");
+
+        if (sendto(m_listenSocket, (void*)&msg, sizeof msg, 0,(struct sockaddr *) &sa6, sizeof sa6) == -1)
+            throw std::runtime_error("Sending message failed");
+    }
+
+    std::string ipv4mappedipv6addr = "::ffff:" + address;
+    if (inet_pton(AF_INET6, ipv4mappedipv6addr.data(), &(sa6.sin6_addr)) != 1)
+        throw std::runtime_error("Unrecognized IP address");
+
+    if (sendto(m_listenSocket, (void*)&msg, sizeof msg, 0,(struct sockaddr *) &sa6, sizeof sa6) == -1)
+        throw std::runtime_error("Sending message failed");
+}
+
+void Connector::listen(MessageBuffer &msgBuffer) {
+    int recvMsgSize = 0;
+    std::unique_ptr<Message> msg(new Message);
+    struct sockaddr_in6 sa6;
+
+    sa6.sin6_family = AF_INET6;
+    sa6.sin6_addr = in6addr_any;
+    sa6.sin6_port = htons(static_cast<uint16_t>(m_listenSocket));
+    ssize_t size = sizeof sa6;
+
+    while(true) {
+        if ((recvMsgSize = recvfrom(m_listenSocket, msg.get(), sizeof(*msg.get()), 0, (struct sockaddr *) &sa6, &size)) == -1)
+            throw std::runtime_error("Receiving message failed");
+
+        //msgBuffer.insert(msg);
+    }
 }
