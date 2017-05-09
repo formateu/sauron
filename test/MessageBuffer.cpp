80d9322b08e600e112628757db5d46fd3b92bd5f
Przemysław Kopański <P.Kopanski@stud.elka.pw.edu.pl>
2017-05-09 12:20:10 +0200

    MessageBuffer monitor implementation
    Change-Id: I91acbdff48ac1baa97055ee18a9a72294f9e9286

---
+++ test/MessageBuffer.cpp
@@ -0,0 +1,27 @@
+#include <thread>
+#include <chrono>
+
+#include "../Message.h"
+#include "../MessageBuffer.h"
+
+BOOST_AUTO_TEST_CASE(msg_buffer_nospinlock) {
+
+    MessageBuffer msgBuffer;
+
+    // trying to read while buffer is empty
+    std::thread readerThread([&msgBuffer]() {
+        msgBuffer.pop();
+    });
+
+    std::thread writerThread([&msgBuffer]() {
+        // waiting a moment to ensure reader thread executes .pop()
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        msgBuffer.push({"127.0.0.1", Message()});
+    });
+
+    readerThread.join();
+    writerThread.join();
+
+    // spinlock haven't occured
+    BOOST_TEST(true);
+}
