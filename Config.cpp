533954d5ed9c692e8a1ee66172389d3200f786ce
quaster <quaster1303@gmail.com>
2017-05-11 22:51:49 +0200

    Restructure code to make separate executables. Read config for file.
    Change-Id: I03ca8fae443863634abefced1b2904fe5bc62b21

---
+++ Config.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Grzegorz Staniszewski on 11.05.17.
+//
+
+#include "Config.h"
+
+Config::Config(const std::string& filePath)
+    : filePath(filePath)
+    , clientSleepSeconds(CLIENT_SLEEP_SECONDS_DEFAULT)
+    , clientWorkSeconds(CLIENT_WORK_SECONDS_DEFAULT) 
+{}
+
+void Config::read() {
+    YAML::Node config = YAML::LoadFile(filePath);
+
+    //TODO: add data validation
+    if (config["client-sleep"]) {
+        clientSleepSeconds = config["client-sleep"].as<long>();
+    }
+
+    if (config["client-work"]) {
+        clientWorkSeconds = config["client-work"].as<long>();
+    }
+}
