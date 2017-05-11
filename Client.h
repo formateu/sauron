34d179be6a96c9e2d8aa9f5633386ccb8fa11a38
quaster <quaster1303@gmail.com>
2017-05-11 18:55:31 +0200

    Refactor client state and handlers.
    Change-Id: I580936ff504b924aaa287f0dab9ac7ba960959a6

--- Client.h
+++ Client.h
@@ -5,52 +5,85 @@
 #define SAURON_CLIENT_H
 
 #include <thread>
+#include <unordered_map>
 
 #include "Connector.h"
 #include "MessageBuffer.h"
+#include "ClientState.h"
 
 class Client {
 public:
-    Client(MessageBuffer &msgBuffer, const std::string &address, size_t port);
+    Client(MessageBuffer &msgBuffer,
+           const std::string &address,
+           size_t port,
+           ClientState state = ClientState::INIT_PHASE_FIRST);
+
     void run();
 
 private:
     Connector connector;
+
     const std::string &mAddress;
+
     MessageBuffer &msgBuffer;
-    int state;
-    bool finish;
+
+    ClientState state;
+
     std::string predecessor;
+
     std::string successor;
+
+    const std::unordered_map<ClientState, std::function<void(const MessagePair &)>> stateRouter = {
+        {
+            ClientState::INIT_PHASE_FIRST,
+            [this](const auto& messagePair) { handleStateInitPhaseFirst(messagePair); }
+        },
+        {
+            ClientState::INIT_PHASE_SECOND,
+            [this](const auto& messagePair) { handleStateInitPhaseSecond(messagePair); }
+        },
+        {
+            ClientState::INIT_PHASE_THIRD,
+            [this](const auto &messagePair) { handleStateInitPhaseThird(messagePair); }
+        },
+        {
+            ClientState::CONNECTION_ESTABLISHED,
+            [this](const auto &messagePair) { handleStateConnectionEstablished(messagePair); }
+        },
+        {
+            ClientState::FINISHING,
+            [this](const auto& messagePair) { handleFinishing(messagePair); }
+        }
+    };
 
     /**
      * Awating for predecessor's initialization.
      * @param messagePair
      */
-    void handleStateInitPhaseFirst(MessagePair messagePair);
+    void handleStateInitPhaseFirst(const MessagePair &messagePair);
 
     /**
      * Awating for successor's initialization.
      * @param messagePair
      */
-    void handleStateInitPhaseSecond(MessagePair messagePair);
+    void handleStateInitPhaseSecond(const MessagePair &messagePair);
 
     /**
      * Awating for ack after successor's initialization.
      * @param messagePair
      */
-    void handleStateInitPhaseThird(MessagePair messagePair);
+    void handleStateInitPhaseThird(const MessagePair &messagePair);
 
     /**
      * Connection has been established properly.
      * @param messagePair
      */
-    void handleStateConnectionEstablished(MessagePair messagePair);
+    void handleStateConnectionEstablished(const MessagePair &messagePair);
 
     /**
      * Handle error message in the first place.
      */
-    void handleFinish(MessagePair messagePair);
+    void handleFinishing(const MessagePair &messagePair);
 };
 
 
