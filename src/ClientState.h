28857f3868e13099f85b3a4b41b2b1a6bf8a5812
Wiktor Franus <killer982@o2.pl>
2017-05-31 21:04:45 +0200

    Remove from enum unused client states
    Change-Id: I894aba84d6616fd56a4746dd3bb8a1b86cd7a37b

--- src/ClientState.h
+++ src/ClientState.h
@@ -11,21 +11,17 @@ enum class ClientState {
     INIT_PHASE_FIRST,
     INIT_PHASE_SECOND,
     INIT_PHASE_LAST_SECOND,
-    INIT_PHASE_THIRD,
     CONNECTION_ESTABLISHED,
     MEASURE_TIME,
     FINISH,
-    TERMINATED
 };
 
 const std::unordered_map<ClientState, std::string> stateStringMap = {
     { ClientState::INIT_PHASE_FIRST,       "Init phase first" },
     { ClientState::INIT_PHASE_SECOND,      "Init phase second" },
-    { ClientState::INIT_PHASE_THIRD,       "Init phase third" },
     { ClientState::CONNECTION_ESTABLISHED, "Connection established" },
     { ClientState::MEASURE_TIME,           "Connection established" },
-    { ClientState::FINISH,                 "Finish" },
-    { ClientState::TERMINATED,             "Terminated" }
+    { ClientState::FINISH,                 "Finish" }
 };
 
 std::ostream &operator<<(std::ostream &stream, const ClientState &state);
